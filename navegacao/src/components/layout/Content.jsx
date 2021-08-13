ddc5082af194328008ae72add48e9270c6e9931c
gabriel7mendes <andrewsseven@gmail.com>
2021-08-13 16:59:50 -0300

    52. Estrutura do projeto

---
+++ navegacao/src/components/layout/Content.jsx
@@ -0,0 +1,6 @@
+import "./Content.css";
+import React from "react";
+
+const Content = (props) => <main className="Content">Componente Content</main>;
+
+export default Content;
---
+++ navegacao/src/views/App.jsx
@@ -0,0 +1,6 @@
+import "./App.css";
+import React from "react";
+
+const App = (props) => <div className="App">Componente App</div>;
+
+export default App;
