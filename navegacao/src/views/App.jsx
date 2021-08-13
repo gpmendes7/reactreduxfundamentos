c890d7135699c9c47c96306702e569096e3f2572
gabriel7mendes <andrewsseven@gmail.com>
2021-08-13 17:50:11 -0300

    53. Aplicando estilo

--- navegacao/src/views/App.jsx
+++ navegacao/src/views/App.jsx
@@ -1,6 +1,13 @@
 import "./App.css";
 import React from "react";
+import Menu from "../components/layout/Menu";
+import Content from "../components/layout/Content";
 
-const App = (props) => <div className="App">Componente App</div>;
+const App = (props) => (
+  <div className="App">
+    <Menu />
+    <Content />
+  </div>
+);
 
 export default App;
