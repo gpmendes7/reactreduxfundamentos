24c645abf257fc99e375a745d8964b32d8405dc3
gabriel7mendes <andrewsseven@gmail.com>
2021-12-22 15:57:52 -0300

    96. Componente com Estado

--- nextjs/src/pages/index.jsx
+++ nextjs/src/pages/index.jsx
@@ -17,6 +17,7 @@ export default function Inicio() {
       <Navegador texto="JSX" destino="/jsx" cor="crimson" />
       <Navegador texto="Navegação #01" destino="/navegacao" cor="green" />
       <Navegador texto="Navegação #02" destino="/cliente/sp-2/321" cor="blue" />
+      <Navegador texto="Componente com Estado" destino="/estado" cor="pink" />
     </div>
   );
 }
