983e2675ccaa47c3f8a45b91794719f5d57333a6
gabriel7mendes <andrewsseven@gmail.com>
2021-12-22 15:26:47 -0300

    93. Componente Navegador

---
+++ nextjs/src/pages/index.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Navegador from "../components/Navegador";
+
+export default function Inicio() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        height: "100vh",
+      }}
+    >
+      <Navegador texto="Estiloso" destino="/estiloso" />
+      <Navegador texto="Exemplo" destino="/exemplo" cor="#9400d3" />
+      <Navegador texto="JSX" destino="/jsx" cor="crimson" />
+    </div>
+  );
+}
