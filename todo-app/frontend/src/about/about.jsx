0263bfd9a00872bfe123e9b19338b572052832e1
gabriel7mendes <andrewsseven@gmail.com>
2022-01-01 10:35:35 -0300

    137. Componente PageHeader

--- todo-app/frontend/src/about/about.jsx
+++ todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default (props) => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name="Sobre" small="Nós"></PageHeader>
+
+    <h2>Nossa História</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      adipisci perspiciatis quis fugiat modi quam repellat nemo voluptates enim,
+      corrupti numquam dolorem, ab earum tempore dignissimos nulla repudiandae
+      incidunt at?
+    </p>
+    <h2>Missão e Visão</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      adipisci perspiciatis quis fugiat modi quam repellat nemo voluptates enim,
+      corrupti numquam dolorem, ab earum tempore dignissimos nulla repudiandae
+      incidunt at?
+    </p>
+    <h2>Imprensa</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      adipisci perspiciatis quis fugiat modi quam repellat nemo voluptates enim,
+      corrupti numquam dolorem, ab earum tempore dignissimos nulla repudiandae
+      incidunt at?
+    </p>
   </div>
 );
