9e2aa1c562893cedd5f474f07766ec273fc80c0e
gabriel7mendes <andrewsseven@gmail.com>
2022-02-01 15:40:27 -0300

    157. Outros Componentes

---
+++ redux-simples/src/components/Soma.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Card from "./Card";
+
+export default (props) => {
+  return (
+    <Card title="Soma de Números" blue>
+      <div>
+        <span>Resultado: </span>
+        <strong>10</strong>
+      </div>
+    </Card>
+  );
+};
