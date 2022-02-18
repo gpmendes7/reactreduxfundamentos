359d765d58601a4d2d3574201491ff7a7045d647
gabriel7mendes <andrewsseven@gmail.com>
2022-02-18 11:30:31 -0300

    179. Adicionar TODO

--- todo-app/frontend/src/todo/todoActions.js
+++ todo-app/frontend/src/todo/todoActions.js
@@ -8,9 +8,17 @@ export const changeDescription = (event) => ({
 });
 
 export const search = () => {
-  const request = axios.get(`${URL}?sort-createdAt`);
+  const request = axios.get(`${URL}?sort=-createdAt`);
   return {
     type: "TODO_SEARCHED",
+    payload: request,
+  };
+};
+
+export const add = (description) => {
+  const request = axios.post(URL, { description });
+  return {
+    type: "TODO_ADDED",
     payload: request,
   };
 };
